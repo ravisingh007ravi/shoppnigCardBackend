1835ea59db5cf7bec2108a44ca360c133a353f79
Ravi Singh <ravisingh007ravi@gmail.com>
2023-04-14 21:00:59 +0530

    add router modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("hello")
+const express = require('express');
+const mongoose  = require('mongoose');
+const cors = require('cors');
+const route = require('./routes/routes.js')
+require('dotenv').config()
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const DB = process.env.DB;
+const port = process.env.PORT || 5000;
+
+
+mongoose.connect(process.env.MONGODB_URI || DB)
+    .then(() => console.log("Mongoose is Connected😊😊"))
+    .catch((err) => console.log(err));
+
+
+app.use('/', route);
+
+app.listen(port, () => console.log(`Server is Running Succesfully ${port}💕`));
